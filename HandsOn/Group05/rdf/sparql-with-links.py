2870b7f53d4196e134aad6f9f1fe9e5531cc71d0
palomin98 <rubenp98@hotmail.com>
2021-10-21 22:27:40 +0200

    Commit con las consultas SPARQL con links
    Al final solucionamos las dudas y hemos conseguido sacar las consultas SPARQL con los links

--- HandsOn/Group05/rdf/sparql-with-links.py
+++ HandsOn/Group05/rdf/sparql-with-links.py
@@ -29,3 +29,46 @@ q = prepareQuery('''
 
 for s in g.query(q):
     print(s)
+    
+print("------- Properties 2:")
+
+q = prepareQuery('''
+    SELECT DISTINCT ?uriProvincia
+    WHERE {
+  		 ?provincia ns:tieneURIProvi ?uriProvincia.
+    }
+    ''',
+    initNs = {"ns": ns}
+)
+
+for s in g.query(q):
+    print(s)
+    
+print("------- Properties 3:")
+
+q = prepareQuery('''
+    SELECT DISTINCT ?municipio ?uriMunicipio
+    WHERE {
+  		 ?municipio ns:tieneURIMuni ?uriMunicipio.
+    }
+    ''',
+    initNs = {"ns": ns}
+)
+
+for s in g.query(q):
+    print(s)
+    
+print("------- Properties 4:")
+
+q = prepareQuery('''
+    SELECT DISTINCT ?provincia ?municipio ?uriMunicipio 
+    WHERE {
+        ?provincia ns:tieneURIProvi ?uriProvincia.
+  		?municipio ns:tieneURIMuni ?uriMunicipio.
+    }
+    ''',
+    initNs = {"ns": ns}
+)
+
+for s in g.query(q):
+    print(s)
